5f6de8e6a08922f13ad627cc08fe4b0a0f4af807
Darshit <darshit12@yahoo.co.in>
2018-03-11 16:38:54 +0530

    added config vars, added logs in some places, removed the keys

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from telegram.ext import Updater, CommandHandler
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import tempfile
+import os
+import logging
 
-
-alphaVantage_apiKey = 'Key'
-telegramKey = 'Key'
-
+alphaVantage_apiKey = os.environ['ALPHAVANTAGE']
+telegramKey = os.environ['TELEGRAM']
+PORT = int(os.environ.get('PORT', '8443'))
 
 def start(bot, update):
     getHelp(bot, update)
 
 
-def getHelp(bot, update):
+def getHelp(bot, update):    
     helpMessage = "/daily: Fetches the daily High, Low, Open, Close, Volume for a company listed on NSE \n\ne.g. <pre> /daily IDFC </pre>\n\n/crypto: Fetches the graph for Cryptocurrency in INR \n\ne.g. <pre> /crypto BTC </pre>\n\n BETA: \n/graph: Fetches the real time(1 min) graph for the companies listed on NSE. \n\ne.g. <pre> /graph IDFC </pre> shows the intraday graph for IDFC \n\n Disclaimer: This bot is a work in Progress"
     update.message.reply_html(helpMessage)
 
-def getGraph(bot, update, args):
+def getGraph(bot, update, args):   
     
     try:
         stockName = " ".join(args).upper()
@@ -44,23 +47,22 @@ def getDaily(bot, update, args):
     
     try:
         stockName = " ".join(args).upper()
-
         ts = TimeSeries(key=alphaVantage_apiKey, output_format='pandas')
         data, meta_data = ts.get_daily(stockName+".NS", outputsize='compact')
-        openVal, closeVal, highVal, lowVal, volume = data.iloc[-1]
-        
+        openVal, highVal, lowVal, closeVal,  volume = data.iloc[-1]
         message = '''
         <b> {} </b> 
         \n Open: <em>₹{}</em>
         \n Close: <em>₹{}</em>
         \n High: <em>₹{}</em>
-        \n Low: <em>{}₹</em>
+        \n Low: <em>₹{}</em>
         \n Volume: <em>{}</em>  
         '''.format(stockName, openVal, closeVal, highVal, lowVal, volume)
         
         update.message.reply_html(message)
         
-    except:
+    except Exception as e:
+        logging.exception("message")
         message = '''You probably used the incorrect format for the command.\nUse /daily <pre>'companyName' </pre> \nFor more info, please check /help'''
         update.message.reply_html(message)
         
@@ -70,10 +72,10 @@ def getCrypto(bot, update, args):
         cryptoName = " ".join(args).upper()
 
         cc = CryptoCurrencies(key=alphaVantage_apiKey, output_format='pandas')
-        data, meta_data = cc.get_digital_currency_intraday(cryptoName, market='INR')
+        data, meta_data = cc.get_digital_currency_intraday(cryptoName, market='USD')
         
         
-        data['1a. price (INR)'].plot()
+        data['1a. price (USD)'].plot()
         plt.tight_layout()
         plotTitle = 'Intraday price for {}'.format(cryptoName)
         plt.title(plotTitle)
@@ -83,18 +85,18 @@ def getCrypto(bot, update, args):
         img = tmpfile
         tmpfile.flush()
 
-        data, meta_data = cc.get_digital_currency_daily(cryptoName, market='INR')
+        data, meta_data = cc.get_digital_currency_daily(cryptoName, market='USD')
         openINR, openUSD, highINR, highUSD, lowINR, lowUSD, closeINR, closeUSD, volume, marketCapUSD = data.iloc[-1]
         
         message = '''
          <b> {} </b> 
-         \n Open: <em>₹{}</em>
-         \n Close: <em>₹{}</em>
-         \n High: <em>₹{}</em>
+         \n Open: <em>${}</em>
+         \n Close: <em>${}</em>
+         \n High: <em>${}</em>
          \n Low: <em>₹{}</em>
          \n Volume: <em>{}</em>
          \n Market Cap: <em>${}</em> 
-        '''.format(cryptoName, openINR, highINR, lowINR, closeINR, volume, marketCapUSD)
+        '''.format(cryptoName, openUSD, highUSD, lowUSD, closeUSD, volume, marketCapUSD)
         
         update.message.reply_photo(img)
         update.message.reply_html(message)
@@ -123,9 +125,9 @@ def main():
     crypto_handler = CommandHandler('crypto', getCrypto, pass_args=True)
     dispatcher.add_handler(crypto_handler)
     
-    # Start the bot
-    updater.start_polling()
-    
+    updater.start_webhook(listen="0.0.0.0",port=PORT,url_path=telegramKey)
+    updater.bot.set_webhook("https://spricebot.herokuapp.com/" + telegramKey)
+
     # Run the bot until you press Ctrl-C
     updater.idle()
 
